44da42ba02fbfa1efbfabb21056b6515eca81452
MohammadFaysal731 <www.faysal731@gmail.com>
2022-04-20 20:37:43 +0600

    create form in checkout pages

--- src/Pages/Checkout/Checkout.js
+++ src/Pages/Checkout/Checkout.js
@@ -1,10 +1,29 @@
 import React from 'react';
+import { Button, Form } from 'react-bootstrap';
 
 
 const Checkout = () => {
     return (
-        <div>
-            <h1>Please Checkout</h1>
+        <div className='p-5 mt-3 w-50 mx-auto border rounded'>
+            <h3 className='text-success'>Please Checkout</h3>
+            <Form>
+                <Form.Group className="mb-3 " controlId="formBasicName">
+                    <Form.Control type="text" placeholder="Enter Name" />
+                </Form.Group>
+                <Form.Group className="mb-3 " controlId="formBasicEmail">
+                    <Form.Control type="email" placeholder="Enter Email" />
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="formBasicPassword">
+                    <Form.Control type="password" placeholder="Enter Password" />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicPhone">
+                    <Form.Control type="number" placeholder="Enter Phone" />
+                </Form.Group>
+                <Button variant="success" type="submit">
+                    Checkout
+                </Button>
+            </Form>
         </div>
     );
 };
