942a3ecdcc8bbad19f4be9e152e7409a8cc3f2c7
MohammadFaysal731 <www.faysal731@gmail.com>
2022-04-21 13:15:21 +0600

    ans difference between authorizatio & authentication

--- src/Pages/Blogs/Blogs.js
+++ src/Pages/Blogs/Blogs.js
@@ -2,9 +2,36 @@ import React from 'react';
 
 const Blogs = () => {
     return (
-        <div>
-            <h1>Blogs</h1>
-        </div>
+        <div className='row m-2'>
+            <div className="col-12 col-md-3 col-lg-3 border border-success rounded-3 m-2">
+                <h6>*** Difference Between Authorization And Authentication ?</h6>
+                <ol>
+                    <li>Authorization hosa kawek kono kaj koranr jono ba kono resources bavohan korar korte parar permission deyar pokiya. Ar Authentication hosa kono user k access access deyan system. </li>
+                    <li>Authorization hosa user validated ki na sata check kora .Ar Authentication user k verified kora</li>
+                    <li>Authorization karjakom Authentication ar por hoy.Ar Authentication agi hoy ter por Authorization hoy.</li>
+                    <li>Authorization user ar anumote nirdaron kora.Ar Authentication proman nirdaron kora j user sothek ki na.</li>
+                </ol>
+            </div>
+            <div className="col-12 col-md-3  col-lg-3 border border-success rounded-3 m-2">
+                <ol>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                </ol>
+            </div>
+            <div className="col-12 col-md-3  col-lg-3 border border-success rounded-3 m-2">
+                <ol>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                </ol>
+            </div>
+
+        </div >
     );
 };
 
