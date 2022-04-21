20631f9b4fd0df8f5b0f5d96d2cdfd78adc5a1a6
MohammadFaysal731 <www.faysal731@gmail.com>
2022-04-21 10:56:58 +0600

    implement facebook login

--- src/Sheared/SocialLogin/SocialLogin.js
+++ src/Sheared/SocialLogin/SocialLogin.js
@@ -3,23 +3,24 @@ import google from '../../images/logoes/google .png'
 import facebook from '../../images/logoes/facebook-.png'
 import github from '../../images/logoes/github.png'
 import auth from '../../firebase.init';
-import { useSignInWithGithub, useSignInWithGoogle } from 'react-firebase-hooks/auth';
+import { useSignInWithFacebook, useSignInWithGithub, useSignInWithGoogle } from 'react-firebase-hooks/auth';
 import Loading from '../Loading/Loading';
 import { useNavigate } from 'react-router-dom';
 const SocialLogin = () => {
     const navigate = useNavigate();
     const [signInWithGoogle, googleUser, googleSingInLoading, googleSingInError] = useSignInWithGoogle(auth);
+    const [signInWithFacebook, facebookUser, facebookSingInLoading, facebookSingInError] = useSignInWithFacebook(auth);
     const [signInWithGithub, githubUser, githubSingInLoading, githubSingInError] = useSignInWithGithub(auth);
 
     let errorElement;
-    if (googleUser || githubUser) {
+    if (googleUser || githubUser || facebookUser) {
         navigate('/blogs')
     }
-    if (googleSingInLoading || githubSingInLoading) {
+    if (googleSingInLoading || githubSingInLoading || facebookSingInLoading) {
         return <Loading></Loading>
     }
-    if (googleSingInError || githubSingInError) {
-        errorElement = <p className='text-danger'>Error: {googleSingInError?.message}{githubSingInError?.message}</p>
+    if (googleSingInError || githubSingInError || facebookSingInError) {
+        errorElement = <p className='text-danger'>Error: {googleSingInError?.message}{githubSingInError?.message}{facebookSingInError?.message}</p>
     }
 
     return (
@@ -35,7 +36,7 @@ const SocialLogin = () => {
                     <img className='' src={google} alt="" />
                     <span className='ms-2'>Google sing in</span>
                 </button>
-                <button className='btn btn-white d-block w-100 mb-2 border'>
+                <button onClick={() => signInWithFacebook()} className='btn btn-white d-block w-100 mb-2 border'>
                     <img className='rounded-3' src={facebook} alt="" />
                     <span className='ms-2'>Facebook sing in</span>
                 </button>
